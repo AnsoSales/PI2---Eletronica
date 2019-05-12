f933756514d0e69fb9336b02c4a1aa04f02e26d5
Anderson Sales <30873955+AnsoSales@users.noreply.github.com>
2019-05-12 17:22:17 -0300

    Update ultrassonico.ino

--- Codigo_sensores/ultrassonico.ino
+++ Codigo_sensores/ultrassonico.ino
@@ -3,23 +3,10 @@
 
 float tempo,dist,media = 0;
 
-//void IRAM_ATTR gpio_isr_handler_up(void* arg)
-//{
-//  
-//  portYIELD_FROM_ISR();
-//  }
-
-
 void ultrassonico(gpio_num_t A, gpio_num_t B)
 {
   gpio_set_direction(A, GPIO_MODE_OUTPUT);
   gpio_set_direction(B, GPIO_MODE_INPUT);
- // gpio_set_intr_type(B, GPIO_INTR_HIGH_LEVEL);
-  //gpio_set_pull_mode(B, GPIO_PULLUP_ONLY);
- // gpio_intr_enable(B);
- // gpio_install_isr_service(0);
-//  gpio_isr_handler_add(B, gpio_isr_handler_up, (void*) B);
-//  timer = timerBegin(0, 80, true);
     }
 void sonico(gpio_num_t A, gpio_num_t B)
 {
@@ -35,15 +22,14 @@ void sonico(gpio_num_t A, gpio_num_t B)
 
 
 void setup() {
-  // put your setup code here, to run once:
-  
+ 
 Serial.begin(115200);
 ultrassonico(trigger,echo);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
   sonico(trigger,echo);
   
   dist= tempo*0.034/2;
