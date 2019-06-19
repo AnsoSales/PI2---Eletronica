a17893cb3d1fff147ceb153a57fd7575797d40a9
Bruno alves ferreira camargos - 150120117 <37155515+bruno2935@users.noreply.github.com>
2019-06-18 22:50:37 -0300

    Update rotinas_alco_oleo_AguaQuente_essencia_e_defini_oes_entradaSaida.ino

--- Codigo_da_maquina/rotinas_alco_oleo_AguaQuente_essencia_e_defini_oes_entradaSaida.ino
+++ Codigo_da_maquina/rotinas_alco_oleo_AguaQuente_essencia_e_defini_oes_entradaSaida.ino
@@ -13,7 +13,7 @@ void configura_bomba_solenoide()
     gpio_set_direction(bomba3 , GPIO_MODE_OUTPUT);//configura bomba3 p/ BOMBA Do alcool
     digitalWrite(bomba3, LOW);
     gpio_set_direction(bomba4 , GPIO_MODE_OUTPUT);//configura bomba4 p/ BOMBA Da essencia1
-    digitalWrite(bomba3, LOW);
+    digitalWrite(bomba4, LOW);
     gpio_set_direction(bomba5 , GPIO_MODE_OUTPUT);//configura bomba4 p/ BOMBA Da essencia2
     digitalWrite(bomba5, LOW);
     gpio_set_direction(bomba6 , GPIO_MODE_OUTPUT);//configura bomba4 p/ BOMBA Da agua quente
@@ -22,7 +22,7 @@ void configura_bomba_solenoide()
     gpio_set_direction(valvula1 , GPIO_MODE_OUTPUT);//configura valvula p/  VALVULA Da balança
     digitalWrite(valvula1, LOW);
     gpio_set_direction(valvula2 , GPIO_MODE_OUTPUT);//configura valvula p/  VALVULA Da agua quente
-    digitalWrite(valvula1, LOW);
+    digitalWrite(valvula2, LOW);
     
 
     gpio_set_direction(motor , GPIO_MODE_OUTPUT);//configura bomba4 p/ BOMBA Da agua quente
