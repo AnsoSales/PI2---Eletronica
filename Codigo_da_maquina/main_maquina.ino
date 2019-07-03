7a41ee78e082f7cdad9aecc24d423b651a295168
Anderson Sales <30873955+AnsoSales@users.noreply.github.com>
2019-07-03 11:17:21 -0300

    Update main_maquina.ino

--- Codigo_da_maquina/main_maquina.ino
+++ Codigo_da_maquina/main_maquina.ino
@@ -166,6 +166,23 @@ void configura_bomba_solenoide()
 
       
 }
+int temp_arduino()
+{
+        while (MySerial.available() > 0) {
+        int byteFromSerial = MySerial.read();
+        // Do something
+        if(byteFromSerial == 4)
+        {
+           MySerial.write(1);
+           digitalWrite(bomba3, LOW);
+         }
+         else
+         {
+            MySerial.write(0); 
+            
+        }
+  return(byteFromSerial = 4)?0:1;
+}
 
 void rotina_oleo()
 {
@@ -193,6 +210,7 @@ void rotina_agua_quente()
     characteristicTX5->notify();
 
     enche_recipiente();//AGORA VEM A COMUNICAÇÃO COM ARDUINO
+    while(temp_arduino()=0){};
     
     digitalWrite(bomba6, HIGH);
     delay(20000);             //tempo em milisegundos necessario para escoar o volume definido
